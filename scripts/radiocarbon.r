4e9a1efa97cc83017c1aba12c6b542d3dbf6419a
zazedd <leomendesantos@gmail.com>
2024-09-02 16:17:41 +0100

    now it will work

--- scripts/radiocarbon.r
+++ scripts/radiocarbon.r
@@ -4,8 +4,8 @@ library(ggplot2)
 args <- commandArgs(trailingOnly = TRUE)
 no_trailing_args <- commandArgs(trailingOnly = FALSE)
 script_name_index <- which(grepl("^--file=", no_trailing_args))
-script_name <- sub("^--file=*scripts/", "", no_trailing_args[script_name_index])
-script_name <- sub("\\.r$", "", script_name)
+script_name <- sub("^--file=", "", no_trailing_args[script_name_index])
+script_name <- sub("\\.r$", "", basename(script_name))
 
 print(script_name)
 
